a1cfc9552c404d53e5468b86e5ee8515040c2aae
Ralf Jung <post@ralfj.de>
2018-09-19 17:22:03 +0200

    add the lint back to the list, and fix tests

--- src/test/ui/consts/const-eval/pub_const_err_bin.rs
+++ src/test/ui/consts/const-eval/pub_const_err_bin.rs
@@ -14,8 +14,4 @@
 pub const Z: u32 = 0 - 1;
 //~^ WARN this constant cannot be used
 
-pub type Foo = [i32; 0 - 1];
-//~^ WARN attempt to subtract with overflow
-//~| WARN this array length cannot be used
-
 fn main() {}
